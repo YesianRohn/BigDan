d3deb0c6fd01f4d1abb0b14de014aace046036ae
1458836984@qq.com <you@example.com>
2023-05-06 05:06:29 +0000

    update sh

--- sh/cmd.sh
+++ sh/cmd.sh
@@ -1,7 +1,8 @@
 # path
 data_path="../../share/course23/aicourse_dataset_final"
 dataset_list=("10shot_cifar100_20200721" "10shot_country211_20210924" "10shot_food_101_20211007" "10shot_oxford_iiit_pets_20211007" "10shot_stanford_cars_20211007")
-model="deit_tiny_patch16_224" # CHANGE ME
+model_list=("deit_tiny_patch16_224" "efficientnet_b3") # ADD ME
+model="${model_list[1]}" # CHANGE ME
 output_dir="output/${model}"
 output_dir_task1=("${output_dir}/${dataset_list[0]}" "${output_dir}/${dataset_list[1]}" "${output_dir}/${dataset_list[2]}" "${output_dir}/${dataset_list[3]}" "${output_dir}/${dataset_list[4]}")
 output_dir_task2="${output_dir}/known_data_source"
@@ -32,7 +33,7 @@ python main.py --data-path ${data_path} --dataset_list ${dataset_list[2]} --mode
 
 # task1-4
 python main.py --data-path ${data_path} --dataset_list ${dataset_list[3]} --model ${model} --output_dir ${output_dir_task1[3]} --batch-size ${bach_size} --epochs ${epochs} --lr ${lr} --weight-decay ${weight_decay} > ${stdout_log}
-python main.py --data-path ${data_path} --dataset_list ${dataset_list[3]} --model ${model} --output_dir ${output_dir_task1[3]} --batch-size ${bach_size} --epochs ${epochs} --lr ${lr} --weight-decay ${weight_decay} --test_only --resume ${resume_task1[3]}/${resume} > ${stdout_log}
+python main.py --data-path ${data_path} --dataset_list ${dataset_list[3]} --model ${model} --output_dir ${output_dir_task1[3]} --batch-size ${bach_size} --epochs ${epochs} --lr ${lr} --weight-decay ${weight_decay} --test_only --resume ${resume_task1[3]} > ${stdout_log}
 
 # task1-5
 python main.py --data-path ${data_path} --dataset_list ${dataset_list[4]} --model ${model} --output_dir ${output_dir_task1[4]} --batch-size ${bach_size} --epochs ${epochs} --lr ${lr} --weight-decay ${weight_decay} > ${stdout_log}
--- sh/para.sh
+++ sh/para.sh
@@ -1,7 +1,8 @@
 # path
 data_path="../../share/course23/aicourse_dataset_final"
 dataset_list=("10shot_cifar100_20200721" "10shot_country211_20210924" "10shot_food_101_20211007" "10shot_oxford_iiit_pets_20211007" "10shot_stanford_cars_20211007")
-model="deit_tiny_patch16_224" # CHANGE ME
+model_list=("deit_tiny_patch16_224" "efficientnet_b3") # ADD ME
+model="${model_list[1]}" # CHANGE ME
 output_dir="output/${model}"
 output_dir_task1=("${output_dir}/${dataset_list[0]}" "${output_dir}/${dataset_list[1]}" "${output_dir}/${dataset_list[2]}" "${output_dir}/${dataset_list[3]}" "${output_dir}/${dataset_list[4]}")
 output_dir_task2="${output_dir}/known_data_source"
